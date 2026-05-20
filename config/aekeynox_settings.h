125df52c5a0646df4ba190a39c19044fcb3a5791
Vincent RENARD <renard.vincent@protonmail.com>
2026-05-20 22:15:54 +0200

    add ergol

---
+++ config/aekeynox_settings.h
@@ -0,0 +1,19 @@
+// Ækeynox settings for Totem
+// See https://github.com/OneDeadKey/zmk-config-aekeynox/blob/main/include/aekeynox/settings.h
+
+// Host Computer
+#define KB_LAYOUT_AZERTY
+// #define MACOS
+// #define LINUX
+
+// Layout Emulation
+#define KB_EMULATION_ERGOL
+
+// Hold-Taps
+#define HT_HOME_ROW_MODS
+#define TAPPING_TERM 200
+#define QUICK_TAP 150
+
+// Other Options
+// #define VIM_NAVIGATION
+// #define HRM_SHIFT
