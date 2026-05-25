04c7f2811426e93ae3c82c0056fc6b3161644d6a
Vincent RENARD <renard.vincent@protonmail.com>
2026-05-26 01:10:44 +0200

    test fix keymap

--- config/aekeynox_settings.h
+++ config/aekeynox_settings.h
@@ -1,19 +1,41 @@
 // Ækeynox settings for Totem
 // See https://github.com/OneDeadKey/zmk-config-aekeynox/blob/main/include/aekeynox/settings.h
 
-// Host Computer
+/*******************************************************************************
+ * Host Computer (keyboard layout and OS)
+ ******************************************************************************/
+
+// Host computer keyboard layout
 #define KB_LAYOUT_AZERTY
+
+// OS (uncomment if not Windows)
 // #define MACOS
 // #define LINUX
 
-// Layout Emulation
+/*******************************************************************************
+ * Layout Emulation
+ ******************************************************************************/
+
+// Emulate Ergo-L
 #define KB_EMULATION_ERGOL
 
-// Hold-Taps
+/*******************************************************************************
+ * Hold-Taps
+ ******************************************************************************/
+
+// Use Home Row Mods (HRM)
 #define HT_HOME_ROW_MODS
+
+// Timing (ms)
 #define TAPPING_TERM 200
 #define QUICK_TAP 150
 
-// Other Options
+/*******************************************************************************
+ * Other Options
+ ******************************************************************************/
+
+// Uncomment for Vim-style navigation
 // #define VIM_NAVIGATION
+
+// Uncomment for Shift as pinky HRM (not recommended for text)
 // #define HRM_SHIFT
